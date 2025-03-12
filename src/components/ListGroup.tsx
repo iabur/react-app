270eb0e3af9b4cee47f04d1a0412ba4d0d3a9c24
iabur <iaburrahman@brainstation-23.com>
2025-03-12 18:01:43 +0600

    Refactor list item click event handling with TypeScript MouseEvent

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,16 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent): void => console.log(event);
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No item found</p>}
-        {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log(item, index)}
-          >
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
