00e41c09089806ce8054526d3ac487e53507c02f
iabur <iaburrahman@brainstation-23.com>
2025-03-12 19:01:04 +0600

    Add onSelectItem callback to ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   const heading = "Cities";
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading={heading} />
+      <ListGroup
+        items={items}
+        heading={heading}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
