985ff796e5d4cb64cf2c84c5cf62499974d96ea0
iabur <iaburrahman@brainstation-23.com>
2025-03-13 13:53:53 +0600

    Add alert component with close functionality; update App to show alert on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <div>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>
+          <h1>Hello</h1>
+        </Alert>
+      )}
       <Button
         color="success"
         text="Click me"
-        onClick={() => console.log("clicked")}
+        onClick={() => setShowAlert(true)}
       />
     </div>
   );
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,20 @@
 import { ReactNode } from "react";
 interface AlertProps {
   children: ReactNode;
+  onClose: () => void;
 }
 
-function Alert({ children }: AlertProps) {
-  return <div className="alert alert-primary">{children}</div>;
+function Alert({ children, onClose }: AlertProps) {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        className="btn-close"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
 }
 
 export default Alert;
