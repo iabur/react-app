1e0d2c1cbfa97256c971d43f9d5b8ba71ec0f0bf
iabur <iaburrahman@brainstation-23.com>
2025-03-14 19:06:26 +0600

    Update App component to replace ExpandableText with Form; import Bootstrap CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form/Form";
 
-const lorem500 =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consequuntur necessitatibus sapiente quibusdam voluptates minus natus, enim illum saepe eius corrupti accusamus animi, rerum placeat voluptate reiciendis optio nihil vel delectus. Doloribus asperiores voluptatum, ducimus vero obcaecati sapiente error suscipit minus placeat rerum delectus enim veniam mollitia iste dolorem officiis?";
 function App() {
   return (
     <div>
-      <ExpandableText text={lorem500} maxLength={10} />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,27 @@
+import "./index.css";
+
+const Form = () => {
+  return (
+    <div className="form-container">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input type="text" className="form-control" id="name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Age
+          </label>
+          <input type="number" className="form-control" id="age" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
