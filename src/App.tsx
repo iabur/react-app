38a2e671bd458d311be065048c8e961931533786
iabur <iaburrahman@brainstation-23.com>
2025-03-17 18:00:47 +0600

    Refactor App component to manage expenses state and integrate ExpenseList for displaying and deleting expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Form from "./components/Form/Form";
-
+import { useState } from "react";
+import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
+    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 30, category: "Entertainment" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
   return (
     <div>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={handleDelete} />
     </div>
   );
 }
