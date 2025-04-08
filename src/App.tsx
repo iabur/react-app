4885448597e6cf877e07ba3e7b67f1e54fe57f60
iabur <iaburrahman@brainstation-23.com>
2025-04-08 14:45:23 +0600

    Refactor App component to establish a connection to a chat server; implement cleanup logic to prevent memory leaks on unmount

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
 
 function App() {
-  const [category, setCategory] = useState<string>("");
+  const connect = () => console.log("connecting to server");
+  const disconnect = () => console.log("disconnecting from server");
+  useEffect(() => {
+    connect();
+    // Cleanup function to disconnect from the server
+    // when the component unmounts
+    // or when the dependencies change
+    // This is important to prevent memory leaks
+    // and to ensure that the connection is closed properly
 
-  return (
-    <div>
-      <h1>Select Category</h1>
-      <select
-        className="form-option"
-        onClick={(event) =>
-          setCategory((event.target as HTMLSelectElement).value)
-        }
-      >
-        <option value=""></option>
-        <option value="Household">Household</option>
-        <option value="Clothing">Clothing</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+    return () => disconnect();
+  }, []);
+
+  return <div>Chat server</div>;
 }
 
 export default App;
