4935d38ab048aa1dc5472a1adb1cf3825efee582
iabur <iaburrahman@brainstation-23.com>
2025-05-06 17:04:31 +0600

    Add loading state to user data fetching; display loading spinner while fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,23 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // cancelling fetch request using AbortController
-
+    setIsLoading(true);
     const controller = new AbortController();
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
       .catch((err) => {
-        if (err instanceof AxiosError) return;
+        if (err instanceof AxiosError && err.name === "CanceledError") return;
         setError(err.message);
+        setIsLoading(false);
       });
 
     return () => {
@@ -32,6 +36,13 @@ function App() {
   return (
     <>
       {error && <div className="text-danger">{error}</div>}
+      {isLoading && (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
